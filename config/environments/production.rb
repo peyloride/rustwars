3f3123930e6a3cb1c9c400a5716dfebddf79e2fa
Okan Binli <okanb3@gmail.com>
2017-08-10 18:47:01 +0300

    background image fix for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Background image fix for heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
